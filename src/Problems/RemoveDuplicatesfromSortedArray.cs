8684d1aba5685f2729a7d7fb8de515791936279e
Spoomer <info@renecodes.eu>
2021-11-02 21:49:34 +0100

    solved RemoveDuplicatesfromSortedArray

---
+++ src/Problems/RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class RemoveDuplicatesfromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums is null || nums.Length == 0 )
+            {
+                return 0;
+            }
+            int uniqueIndex = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i-1] != nums[i])
+                {
+                    uniqueIndex++;
+                    nums[uniqueIndex] = nums[i];
+                }
+            }
+            return uniqueIndex + 1;
+        }
+    }
+}
