b0cda0840b4ec79f574433ecb9b1d132bd34de7e
Spoomer <info@renecodes.eu>
2021-11-03 21:06:28 +0100

    solved strstr

---
+++ LeetCode.Test/ImplementStrStrTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LeetCode.Problems;
+using Xunit;
+
+namespace LeetCode.Test
+{
+
+    public class ImplementStrStrTests
+    {
+        [Fact]
+        public void ImplementStrStrTest1()
+        {
+            ImplementStrStrProb str = new();
+            Assert.Equal(2, str.StrStr("hello", "ll"));
+        }
+        [Fact]
+        public void ImplementStrStrTest2()
+        {
+            ImplementStrStrProb str = new();
+            Assert.Equal(-1, str.StrStr("aaaa", "aab"));
+        }
+        [Fact]
+        public void ImplementStrStrTest3()
+        {
+            ImplementStrStrProb str = new();
+            Assert.Equal(0, str.StrStr("", ""));
+        }
+        [Fact]
+        public void ImplementStrStrTest4()
+        {
+            ImplementStrStrProb str = new();
+            Assert.Equal(6, str.StrStr("asdasdab", "ab"));
+        }
+    }
+}
---
+++ src/Problems/ImplementStrStrProb.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class ImplementStrStrProb
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (String.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    int end = i + needle.Length;
+                    if (haystack[i..].Length >= needle.Length && haystack[i..end] == needle)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
