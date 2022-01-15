914f8606315d715d1938f1a663a77b61c099f0db
Spoomer <info@renecodes.eu>
2022-01-15 20:37:52 +0100

    solved: group anagrams

---
+++ LeetCode.Test/GroupAnagramsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace LeetCode.Test;
+public class GroupAnagramsTests
+{
+    [Fact]
+    public void ShouldBeEqual_GroupAnagrams()
+    {
+        var input = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        IList<IList<string>> expected = new string[][] 
+                                        { 
+                                            new string[] { "ate", "eat", "tea" },
+                                            new string[] { "nat", "tan" },
+                                            new string[] { "bat" }
+                                        };
+        var actual = GroupAnagramsProb.GroupAnagrams(input);
+
+        bool compareAll = true;
+        foreach(var actualItem in actual)
+        {
+            var sorted = actualItem.OrderBy(x=>x);
+            foreach(var expectedItem in expected)
+            {
+                if(expectedItem.SequenceEqual(sorted))
+                {
+                    compareAll = true;
+                    break;
+                }
+                else
+                {
+                    compareAll &= false;
+                }
+            }
+        }
+        Assert.True(compareAll);
+    }
+}
---
+++ src/Problems/GroupAnagramsProb.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems;
+
+public class GroupAnagramsProb
+{
+    public static IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, int> indexlist = new();
+        List<IList<string>> result = new();
+        if (strs.Length == 0)
+        {
+            result.Add(new List<string>() { "" });
+            return result;
+        }
+        foreach (string str in strs)
+        {
+            string sortedStr = Tools.SortString(str);
+            if (indexlist.TryGetValue(sortedStr, out int i))
+            {
+                result[i].Add(str);
+            }
+            else
+            {
+                result.Add(new List<string>() { str });
+                indexlist.Add(sortedStr, result.Count - 1);
+            }
+        }
+        return result;
+    }
+
+}
